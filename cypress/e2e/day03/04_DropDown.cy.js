4fe65e450214cdd5210bd2660a318acede382c54
Emre MENTES <emreowen57@gmail.com>
2025-12-30 21:42:18 +0100

    changes

--- cypress/e2e/day03/04_DropDown.cy.js
+++ cypress/e2e/day03/04_DropDown.cy.js
@@ -5,7 +5,7 @@ describe('DropDown', () => {
     it('Test Case', () => {
 
         cy.visit('https://the-internet.herokuapp.com/dropdown')
-        cy.get('#dropdown').select('Option 1').should('have.value', '1')
+       /cy.get('#dropdown').select('Option 1').should('have.value', '1')
 
         cy.get('#dropdown').select('Option 2').should('have.value', '2')
 
