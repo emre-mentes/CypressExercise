2f6a2c9a8d8a9b84f64c34903d659d40b69ce450
Emre MENTES <emreowen57@gmail.com>
2025-12-30 22:58:56 +0100

    changes

---
+++ cypress/e2e/day04/05_MultipleWindows.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('Multiple Windows', () => {
+
+    it('Removing Attribute', () => {
+        cy.visit('https://the-internet.herokuapp.com/windows')
+
+        cy.get('.example > a').invoke('removeAttr', 'target').click()
+
+        cy.get('h3').should('have.text', 'New Window')
+    })
+
+    it.only('New Url', () => {
+        cy.visit('https://the-internet.herokuapp.com/windows')
+        cy.get('.example > a').then((element) => {
+            const newURL = element.prop('href')
+            //prop() -> 'href' değerini ('/windows/new') aldik
+            // prop(), jQuery'de attribute değerini alan bir fonksiyon
+
+        cy.visit(newURL)
+        })
+    })
+})
